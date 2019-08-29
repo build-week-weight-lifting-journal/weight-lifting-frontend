1a39d9cb5cd0273b59373fc8831acd9e920ea245
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-08-29 03:00:30 -0500

    Search bar functioning. Styling for ExerciseList complete.

--- lambda-lifts/src/components/ExerciseList.js
+++ lambda-lifts/src/components/ExerciseList.js
@@ -5,6 +5,7 @@ import { getExerciseData, deleteWorkout, postExercise } from "../actions/index";
 import "../ExerciseList.scss";
 
 const ExerciseList = props => {
+
   const [exerciseObject, setExerciseObj] = useState(
     {
       weight: "100kg",
@@ -12,6 +13,12 @@ const ExerciseList = props => {
       sets: 10,
     }
   );
+
+  const [search, setSearch] = useState("");
+
+  const searchHandler = e => {
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
     props.getExerciseData();
@@ -24,34 +31,66 @@ const ExerciseList = props => {
 
   return (
     <div>
-      <div className="bodyNav">
-        <Link to="/WorkoutList">
-          <p onClick={props.deleteWorkout}>Cancel</p>
-        </Link>
-        <p>Add Exercises</p>
-        <Link to="/RepSets">
-          <p onClick={() => props.postExercise(exerciseObject)}>Save</p>
-        </Link>
+      <div className="nav-container">
+        <div className="exercise-nav">
+          <Link to="/WorkoutList">
+            <p onClick={props.deleteWorkout}>Cancel</p>
+          </Link>
+          <p id="add-exercises">Add Exercises</p>
+          <Link to="/RepSets">
+            <p onClick={() => props.postExercise(exerciseObject)}>Save</p>
+          </Link>
+        </div>
       </div>
 
+      <div>
+        <form className="exercise-form">
+          <input
+                type="text"
+                name="search"
+                className="exercise-search"
+                onChange={searchHandler}
+                placeholder="Search Exercise"
+              />
+        </form>
+      </div>
       <div className="exercise-list-container">
-        {props.exercises &&
+        {search ? 
+          props.exercises &&
+          props.exercises.map((exercise, index) => {
+            if (exercise.name.toLowerCase().includes(search)) {
+              return (
+                <button
+                  className="exercises"
+                  key={index}
+                  onClick={() => {
+                    setExerciseObj({
+                      ...exerciseObject,
+                      ["exerciseId"]: exercise.id
+                    });
+                   localStorage.setItem("exerciseId", exercise.id);}}
+                >
+                  {exercise.name}
+                </button>
+              )
+            }  
+          }) 
+        : props.exercises &&
           props.exercises.map((exercise, index) => (
-            <button
-              className="exercises"
-              key={index}
-              onClick={() => {
-                setExerciseObj({
-                  ...exerciseObject,
-                  ["exerciseId"]: exercise.id
-                });
-
-                localStorage.setItem("exerciseId", exercise.id);
-              }}
-            >
-              {exercise.name}
-            </button>
-          ))}
+                <button
+                  className="exercises"
+                  key={index}
+                  onClick={() => {
+                    setExerciseObj({
+                      ...exerciseObject,
+                      ["exerciseId"]: exercise.id
+                    });
+                   localStorage.setItem("exerciseId", exercise.id);}}
+                >
+                  {exercise.name}
+                </button>
+              )
+          )}
       </div>
     </div>
   );
